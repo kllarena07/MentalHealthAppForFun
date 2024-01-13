6f002301ef18ced28132a43faead62c3db1d506b
Kieran Llarena <baymax709@gmail.com>
2024-01-13 13:01:38 -0500

    init: init commit

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'expo-dev-menu'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
-export default function App() {
+export default App = () => {
+  const [text, onChangeText] = useState("");
+  const [todos, mutateTodos] = useState([]);
+
+  const addTodo = (todoName) => {
+    mutateTodos(todos => [...todos, { name: todoName } ])
+  }
+  
+  const removeTodo = (index) => {
+    mutateTodos(todos => {
+      const newTodos = [...todos]
+      newTodos.splice(index, 1);
+      return newTodos
+    })
+  }
+  
+  useEffect(() => {
+    onChangeText("")
+  }, [todos])
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Enter a Todo:</Text>
+      <TextInput onChangeText={onChangeText} placeholder="Enter a todo" value={text} />
+      {text != "" && text.length < 3 ? (
+        <Text style={{
+          color: "red"
+        }}>Your todo must be longer than 3 characters</Text>
+      ) : ""}
+      <Button title="Add Todo" onPress={() => addTodo(text)} disabled={text.length < 3}></Button>
+      {todos.map((item, index) => {
+        return (
+          <View>
+            {/*hacky fix, assign each key a unique index*/}
+            <Text key={`${item.name.slice(0, 2)}_customIndex`}>{item.name}</Text>
+            <Button title="X" onPress={() => removeTodo(index)}></Button>
+          </View>
+        )
+      })}
     </View>
   );
 }
